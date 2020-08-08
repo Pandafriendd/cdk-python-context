b1deff1e17d0b70a5cbd545d04493002b9652154
Li <lizhiyua@38f9d35d438d.ant.amazon.com>
2020-08-08 01:54:58 -0400

    first commit

--- app.py
+++ app.py
@@ -6,6 +6,24 @@ from cdk_python_context.cdk_python_context_stack import CdkPythonContextStack
 
 
 app = core.App()
-CdkPythonContextStack(app, "cdk-python-context", env={'region': 'us-west-2'})
+
+print (app.node.try_get_context('prod')['vpc_id'])
+print (app.node.try_get_context('dev')['vpc_id'])
+
+env_prod = core.Environment(
+    account = app.node.try_get_context('prod')['account'],
+    region = app.node.try_get_context('prod')['region']
+)
+
+env_dev=core.Environment(
+    account = app.node.try_get_context('dev')['account'],
+    region = app.node.try_get_context('dev')['region']
+)
+
+CdkPythonContextStack(app, "cdk-python-context-prod", env=env_prod)
+
+# CdkPythonContextStack(app, "cdk-python-context-dev", env=env_dev)
+
+# CdkWorkshopStack(app, "cdkworkshop", env={'region': 'us-west-2'})
 
 app.synth()
